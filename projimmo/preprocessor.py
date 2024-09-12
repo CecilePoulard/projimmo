8060402619caed3e44b6fa3fbac46715f42f42af
Cecile Poulard <poulardcecile@gmail.com>
2024-09-12 10:07:49 +0200

    Package MAJ

--- projimmo/preprocessor.py
+++ projimmo/preprocessor.py
@@ -20,7 +20,7 @@ def preprocess_features(X: pd.DataFrame) -> pd.DataFrame:
   # Définir les pipelines de prétraitement
     preproc_robust = make_pipeline(RobustScaler())
     preproc_standard = make_pipeline(StandardScaler())
-    preproc_categorical_baseline = make_pipeline(OneHotEncoder(handle_unknown="ignore"))
+    preproc_categorical_baseline = make_pipeline(OneHotEncoder(handle_unknown="ignore", drop='if_binary'))
     preproc_ordinal = make_pipeline(OrdinalEncoder())
 
     # Définir le transformer de colonnes
@@ -34,7 +34,7 @@ def preprocess_features(X: pd.DataFrame) -> pd.DataFrame:
     X_trans = preproc_baseline.fit_transform(X)
 
     # Fonction pour obtenir les noms des colonnes
-    def get_feature_names(column_transformer):
+    def get_feature_names(column_transformer, original_columns):
         feature_names = []
         for name, transformer, columns in column_transformer.transformers_:
             if hasattr(transformer, 'get_feature_names_out'):
@@ -42,17 +42,21 @@ def preprocess_features(X: pd.DataFrame) -> pd.DataFrame:
                 feature_names.extend(transformer.get_feature_names_out())
             else:
                 feature_names.extend(columns)
+        # Ajouter les noms des colonnes non transformées
+
         return feature_names
 
     # Obtenir les noms des colonnes
-    feature_names = get_feature_names(preproc_baseline)
+    feature_names = get_feature_names(preproc_baseline,X.columns)
 
     # Convertir en DataFrame si la sortie est une matrice creuse
     if hasattr(X_trans, "toarray"):
         X_trans_dense = X_trans.toarray()
     else:
         X_trans_dense = X_trans
-
+    # Renommer la dernière colonne en "nombre_de_lots"
+    if len(feature_names) > 0:
+        feature_names[-1] = "nombre_de_lots"
     # Créer la DataFrame avec les noms de colonnes
     X_trans_df = pd.DataFrame(X_trans_dense, columns=feature_names)
     print("✅ X_processed, with shape", X_trans_df.shape)
